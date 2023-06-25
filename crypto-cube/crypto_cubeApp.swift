a07ad84a7889890056abcef4704f6e0fbaf24918
MaciejDubowik <maciejxpoland@gmail.com>
2023-06-25 23:48:35 +0200

    initial commit

---
+++ crypto-cube/crypto_cubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_cubeApp.swift
+//  crypto-cube
+//
+//  Created by Maciej Dubowik on 25/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct crypto_cubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
